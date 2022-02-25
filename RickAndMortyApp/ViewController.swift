e16f9afe1d1768c3390b8f9e2112cd9a4ab69108
Felipe Augusto Silva <felipeas@ciant.com>
2022-02-25 18:41:28 -0300

    feat: adding searchbar

--- RickAndMortyApp/ViewController.swift
+++ RickAndMortyApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    var fieldState: Bool = false
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet weak var searchIcon: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.navigationItem.titleView = searchBar
+        self.navigationItem.titleView?.isHidden = true
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onSearchTap(_ sender: UIBarButtonItem) {
+            if fieldState == false {
+                fieldState = true
+                navigationItem.rightBarButtonItem?.image = UIImage(systemName: "xmark.circle")
+                animateSearchBar()
+                navigationItem.titleView
+            searchBar.setShowsScope(true, animated: true)
+            self.navigationItem.titleView?.isHidden = false
+            } else {
+                fieldState = false
+                searchBar.setShowsScope(true, animated: true)
+                self.navigationItem.titleView?.isHidden = true
+                navigationItem.rightBarButtonItem?.image = UIImage(systemName: "magnifyingglass")
+            }
+        
+    }
+    func animateSearchBar() {
+        let animationTransition = CATransition()
+        animationTransition.duration = 0.1
+        animationTransition.type = CATransitionType.push
+        animationTransition.subtype = CATransitionSubtype.fromBottom
+        searchBar.layer.add(animationTransition, forKey: "pushText")
+    }
+ 
 }
 
