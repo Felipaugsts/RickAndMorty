13b64f9b2abfb579d2af28893f4e08a4b386aa33
Felipe Augusto Silva <felipeas@ciant.com>
2022-02-26 14:06:56 -0300

    feat: adding collection view

---
+++ CharacterCVCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterCVCell.swift
+//  RickAndMortyApp
+//
+//  Created by FELIPE AUGUSTO SILVA on 26/02/22.
+//
+
+import UIKit
+
+class CharacterCVCell: UICollectionViewCell {
+    
+}
--- RickAndMortyApp/ViewController.swift
+++ RickAndMortyApp/ViewController.swift
@@ -9,23 +9,29 @@ import UIKit
 
 class ViewController: UIViewController, UISearchBarDelegate {
     var fieldState: Bool = false
+    @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet weak var CharacterCollectionView: UICollectionView!
     @IBOutlet weak var searchIcon: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.navigationItem.titleView = searchBar
         self.navigationItem.titleView?.isHidden = true
         searchBar.delegate = self
         // Do any additional setup after loading the view.
+
     }
     
+    @IBAction func onIconTap(_ sender: UIBarButtonItem) {
+        print("test")
+    }
     @IBAction func onSearchTap(_ sender: UIBarButtonItem) {
             if fieldState == false {
                 fieldState = true
                 navigationItem.rightBarButtonItem?.image = UIImage(systemName: "xmark.circle")
                 animateSearchBar()
+                
                 navigationItem.titleView
             searchBar.setShowsScope(true, animated: true)
             self.navigationItem.titleView?.isHidden = false
@@ -47,3 +53,17 @@ class ViewController: UIViewController, UISearchBarDelegate {
  
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = CharacterCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CharacterCVCell else {
+            fatalError()
+        }
+        return cell
+    }
+    
+    
+}
